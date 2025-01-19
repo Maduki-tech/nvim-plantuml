be524e15f51bf2d1a6998eff81845db6eb74c42d
Maduki-tech <d.schlueter1011@gmail.com>
2025-01-19 11:08:43 +0100

    added logic to prevent false filetypes

--- lua/plantuml/render.lua
+++ lua/plantuml/render.lua
@@ -2,7 +2,17 @@ local config = require("plantuml.config")
 
 local M = {}
 
+local function isPumlFile()
+	local bufname = vim.api.nvim_buf_get_name(0)
+	return bufname:match("%.puml")
+end
+
 M.preview = function()
+	if not isPumlFile() then
+		vim.notify("Only `.puml` file is supportet for now", vim.log.levels.ERROR, {})
+		return
+	end
+
 	local buf = vim.api.nvim_get_current_buf()
 	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
 	local uml_file = config.output_dir .. "/diagram.puml"
